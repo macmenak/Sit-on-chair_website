7847e8da45b9d419c33eafc811846b377bb68dcd
macmenak <macmenak@gmail.com>
2018-04-18 20:34:02 +0200

    Finish working compose calculator section

--- js/main.js
+++ js/main.js
@@ -34,4 +34,85 @@ document.addEventListener("DOMContentLoaded", function () {
         slides.style.marginLeft = 0-slideNumber*1044+"px";
     });
 
+    var listArrows = document.getElementsByClassName("list_arrow");
+    var listArrowsLength = listArrows.length;
+    var listPanelElements = document.querySelectorAll(".list_panel > li");
+    var listPanelElementsLength = listPanelElements.length;
+
+    var chairNameEl = document.querySelector(".summary_panel .title");
+    var chairPriceEl = document.querySelector(".summary_panel .title.value");
+    var chairPrice = 0;
+    var colorNameEl = document.querySelector(".summary_panel .color");
+    var colorPriceEl = document.querySelector(".summary_panel .color.value");
+    var colorPrice = 0;
+    var patternNameEl = document.querySelector(".summary_panel .pattern");
+    var patternPriceEl = document.querySelector(".summary_panel .pattern.value");
+    var patternPrice = 0;
+    var transportNameEl = document.querySelector(".summary_panel .transport");
+    var transportPriceEl = document.querySelector(".summary_panel .transport.value");
+    var transportPrice = 0;
+    var sumEl = document.querySelector(".summary_panel .sum");
+    var sumPrice = 0;
+
+    function updateSum (){
+        sumPrice = parseInt(chairPrice) + parseInt(colorPrice) + parseInt(patternPrice) + parseInt(transportPrice);
+        sumEl.innerText = sumPrice + " zł";
+    }
+
+    for (var i = 0; i < listArrowsLength; i++) {
+        listArrows[i].addEventListener("click", function () {
+           var thisListPanelEl = this.parentElement.querySelector(".list_panel");
+            thisListPanelEl.classList.toggle("visible");
+        });
+    }
+
+    for (var i = 0; i < listPanelElementsLength; i++) {
+        listPanelElements[i].addEventListener("click", function () {
+            var thisListPanelEl = this.parentElement;
+            var thisDropDownListEl = thisListPanelEl.parentElement;
+            var thisListLabelEl = thisDropDownListEl.querySelector(".list_label");
+            thisListPanelEl.classList.toggle("visible");
+            var dataType = thisListPanelEl.dataset.type;
+            var dataPrice = this.dataset.price;
+
+
+            if(dataType == 'chair'){
+                chairNameEl.innerText = "Chair " + this.innerText;
+                chairPriceEl.innerText = dataPrice;
+                chairPrice = dataPrice;
+            }
+            else if (dataType =='color'){
+                colorNameEl.innerText = this.innerText;
+                colorPriceEl.innerText = dataPrice;
+                colorPrice = dataPrice;
+            }
+            else if (dataType =='pattern'){
+                patternNameEl.innerText = this.innerText;
+                patternPriceEl.innerText = dataPrice;
+                patternPrice = dataPrice;
+            }
+
+            updateSum();
+            thisListLabelEl.innerText = this.innerText;
+            thisListLabelEl.classList.add("selected");
+        });
+    }
+
+    var transportCheckbox = document.getElementById("transport");
+
+    transportCheckbox.addEventListener("click", function () {
+        var isTransportChecked= transportCheckbox.checked;
+        var dataPrice = this.dataset.transportPrice;
+        if(isTransportChecked){
+            transportNameEl.innerText = "Transport";
+            transportPriceEl.innerText = dataPrice;
+            transportPrice = parseInt(dataPrice);
+        } else {
+            transportNameEl.innerText = "";
+            transportPriceEl.innerText = "";
+            transportPrice = 0;
+        }
+        updateSum();
+
+    });
 });
